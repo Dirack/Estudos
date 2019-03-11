ee9b797bca901a5c72dec87673e586af4a6f1a07
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-03-11 08:58:43 -0300

    Estudo sobre isset e empty do PHP, envio de formulários

---
+++ PHP/formularios/index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+	index.php (PHP)
+	
+	Objetivo: Estudo sobre submeter formulário em PHP.
+	
+	Versão 1.0
+	
+	Site: http://www.dirackslounge.online
+	
+	Programador: Rodolfo A. C. Neves (Dirack) 11/03/2019
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: Software de uso livre e código aberto.
+*/
+
+// isset verifica se email foi setado e 
+// empty verifica se a variável está vazia
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+	$email = $_POST['email'];
+
+	echo "usuário enviou os dados!";
+	echo "meu email: ".$email;
+
+}
+
+
+
+?>
+
+<head>
+<title>Estudo sobre submeter formulário em PHP</title>
+</head>
+
+<body>
+
+	<!-- Formulário: envie um email para obter uma resposta -->
+	<hr>
+	<form method="POST">
+		Email:
+		<input type="text" name="email"><br>
+
+		Senha:
+		<input type="text"><br>
+		<input type="submit">
+
+
+	</form>
+</body>
