a7ceb9e39a3e7325a206f990e2d739ab70cbbeb1
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-04-27 23:35:12 -0300

    Estudo sobre laço while em PHP

---
+++ PHP/while/index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+	index.php (PHP)
+	
+	Objetivo: Estudo sobre laço WHILE.
+	
+	Versão 1.0
+	
+	Site: http://www.dirackslounge.online
+	
+	Programador: Rodolfo A. C. Neves (Dirack) 27/04/2019
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: Software de uso livre e código aberto.
+*/
+
+echo "<h1>Estudo sobre laço WHILE</h1>";
+
+$x=0;
+
+while($x<10){
+	echo "X é igual a ".$x."<br>";
+	$x++;
+}
+
+?>
+
