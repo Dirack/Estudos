95187bc95f4b8b6e1b642e74eb1badc5f7799d7b
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-05-08 18:21:54 -0300

    Função time do PHP

--- PHP/funcoes_Date_Time/index.php
+++ PHP/funcoes_Date_Time/index.php
@@ -8,7 +8,9 @@
 	
 	Site: http://www.dirackslounge.online
 	
-	Programador: Rodolfo A. C. Neves (Dirack) 06/05/2019
+	Programador: Rodolfo A. C. Neves (Dirack) 
+			Primeira parte 06/05/2019
+			Segunda parte 08/05/2019
 	
 	Email: rodolfo_profissional@hotmail.com
 	
@@ -21,20 +23,50 @@
 </head>
 <body>
 
-<?php
+	<div class="estudoDate">
+		<h1>Estudo sobre a função DATE em PHP</h1>
+		
+		<?php
 
-/* dia atual de 01-31 */
-$dataAtual=date("d");
+			/* dia atual de 01-31 */
+			$dataAtual=date("d");
 
-echo $dataAtual."<br>";
+			echo "<p>Data atual(dia): ".$dataAtual."</p>";
 
-/* data no formato dd/mm/aaaa e hora hh:ii:ss */
-$dataAtual=date("d/m/Y \à\s H:i:s");
+			/* data no formato dd/mm/aaaa e hora hh:ii:ss */
+			$dataAtual=date("d/m/Y \à\s H:i:s");
 
-echo $dataAtual."<br>";
+			echo "<p>Data no formato (dd/mm/aaaa): ".$dataAtual."</p>";
 
 
-?>
+		?>
+	</div>
+
+	<div class="estudoTime">
+
+		<h1>Estudo sobre a função TIME em PHP</h1>
+
+		<?php
+
+			/* Tempo em segundos desde 01 JAN 1970 */
+			$x = time();
+
+			echo $x."<br>";
+
+			$dataProxima = date('d/m/Y', strtotime('+10 days'));
+
+			echo $dataProxima."<br>";
+
+			$dataProxima = date('d/m/Y', 0);
+
+			echo $dataProxima."<br>";
+
+			$dataProxima = date('d/m/Y', strtotime('+2 days', 0));
+
+			echo $dataProxima."<br>";
+
+		?>
+	</div>
 
 
 </body>
