c3723cbc99fca4686dddc45234d8b3cc02912425
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-05-29 16:57:21 -0300

    Estudo sobre arrays em JAVA

---
+++ Java/arrays/arrays.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class arrays{
+
+	public static void main(String[] args){
+
+		// Array do tipo string
+		String[] nome= new String[5];
+
+		nome[0] = "João";
+		nome[1] = "Rodolfo";
+
+		String[][] nome2= new String[2][2];
+
+		nome2[0][0] = "Rodolfo Dirack";
+
+		System.out.println("Meu nome é "+nome2[0][0]);
+
+
+
+	}
+
+}
