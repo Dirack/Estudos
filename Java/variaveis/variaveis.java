68fac6a54bd65f4b293eb4354d5036d2f682a054
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-05-29 16:51:22 -0300

    Estudo sobre variáveis em java atualizado

--- Java/variaveis/variaveis.java
+++ Java/variaveis/variaveis.java
@@ -4,9 +4,18 @@ public class variaveis{
 
 	public static void main(String[] args){
 
-		int idade=27;
+		// Tipos de variáveis do JAVA
+		final int constanteInteiro=10;
+		int inteiro=27;
+		float numeroReal=2;
+		double numeroRealMaiorPrecisao=2.3333444;
+		char letra='a';
+		boolean booleano=true;
+		String frase="Olá mundo JAVA!";
 
-		System.out.println("Minha idade é "+idade+" anos :)");
+		System.out.println(frase);
+
+		System.out.println("Minha idade é "+inteiro+" anos :)");
 
 	}
 }
