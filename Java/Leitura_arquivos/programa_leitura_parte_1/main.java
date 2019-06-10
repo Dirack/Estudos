8fb52d6d9cc47384c7b0ac73b5fe3f6c6f13b74e
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-06-10 20:20:13 -0300

    Primeira parte do estudo sobre leitura de arquivos em java

---
+++ Java/Leitura_arquivos/programa_leitura_parte_1/main.java
@@ -0,0 +1,38 @@
+/*
+	 main.java (java)
+	 
+	 Objetivo: Estudo sobre leitura de arquivos com Java.
+	 Neste estudo será pedido o nome do usuário e a resposta
+	 será este nome exibido na tela.
+	 
+	 Versão 1.0
+	 
+	 Site: http://www.dirackslounge.online
+	 
+	 Programador: Rodolfo A. C. Neves (Dirack) 10/06/2019
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 Licença: Software de uso livre e código aberto.
+*/
+
+package programa_leitura_parte_1;
+
+import java.util.Scanner;
+
+public class main{
+
+	public static void main(String[] args){
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Digite o seu nome: ");
+
+		String nome = in.nextLine();
+
+		System.out.println("Seu nome é "+nome);
+
+
+	}
+
+}
