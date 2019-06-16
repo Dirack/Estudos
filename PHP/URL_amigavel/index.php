0c00b81183563a3b2eaf40f851fe5fc71d0f4ea0
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-06-16 20:06:41 -0300

    Estudo sobre URL amigável com mod_rewrite do PHP

---
+++ PHP/URL_amigavel/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	index.php (PHP)
+	
+	Objetivo: Estudo sobre mod_rewrite (URL amigável) em PHP.
+	
+	Versão 1.0
+	
+	Site: http://www.dirackslounge.online
+	
+	Programador: Rodolfo A. C. Neves (Dirack) 16/06/2019
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: Software de uso livre e código aberto.
+*/
+
+if(isset($_GET) && !empty($_GET)){
+	print_r($_GET);
+}
+
+
+
+?>
