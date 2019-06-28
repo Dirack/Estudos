93d60acc01f3ed536215d50465850371ddba63a9
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-06-28 12:28:29 -0300

    Estudo sobre como exibir mensagens na tela com jframe do java

---
+++ Java/jframe_mensagem/Main.java
@@ -0,0 +1,25 @@
+/*
+	 mensagem.java (java)
+	 
+	 Objetivo: Ilustrar como exibir uma mensagem na tela co JFrame do java.
+	 
+	 Versão 1.0
+	 
+	 Site: http://www.dirackslounge.online
+	 
+	 Programador: Rodolfo A. C. Neves (Dirack) 28/06/2019
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 Licença: Software de uso livre e código aberto.
+*/
+
+import javax.swing.JOptionPane;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        JOptionPane.showMessageDialog(null, "Exibindo uma mensagem");
+    }
+} 
