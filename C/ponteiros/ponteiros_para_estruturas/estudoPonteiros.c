35e20cfd32ba83f827fedd53b68f9000cd121a8d
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-07-04 15:11:11 -0300

    estudo sobre ponteiros de estruturas em C

---
+++ C/ponteiros/ponteiros_para_estruturas/estudoPonteiros.c
@@ -0,0 +1,38 @@
+/*
+	 estudoPonteiros.c (c)
+	 
+	 Objetivo: Estudo sobre ponteiros de estruturas em C
+	 
+	 Versão 1.0
+	 
+	 Site: http://www.dirackslounge.online
+	 
+	 Programador: Rodolfo A. C. Neves (Dirack) 04/07/2019
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 Licença: Software de uso livre e código aberto.
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+	struct horario{
+		int hora;
+		int minuto;
+		int segundo;
+	};
+
+	struct horario agora;
+	struct horario* depois;
+	depois = &agora;
+
+	(*depois).hora = 20;
+	(*depois).minuto = 30;
+	(*depois).segundo = 54;
+
+	printf("%i:%i:%i\n", agora.hora, agora.minuto, agora.segundo);
+
+}
+	
