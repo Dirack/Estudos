7a9210204e2657461f94455bfcfc7189595d9eed
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-07-12 23:41:11 -0300

    Estudo sobre autenticação HTTP com PHP

---
+++ PHP/validacao_http/admin.php
@@ -0,0 +1,29 @@
+<?php 
+require("./autenticar.php");
+?>
+
+<!doctype html>
+<!--
+	admin.php (PHP)
+	
+	Objetivo: Simular Página do administrador.
+	
+	Site: http://www.dirackslounge.online
+	
+	Versão 1.0
+	
+	Programador: Rodolfo Dirack 12/07/2019
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+-->
+<head>
+	<meta charset="utf-8">
+	<title>Página ADMIN: Estudo sobre autenticação HTTP</title>
+</head>
+<body>
+
+	<h1> Bem vindo Administrador! </h1>
+</body>
+</html>
---
+++ PHP/validacao_http/autenticar.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	autenticar.php (PHP)
+	
+	Objetivo: Estudo sobre autenticação HTTP com PHP.
+	
+	Site: http://www.dirackslounge.online
+	
+	Versão 1.0
+	
+	Programador: Rodolfo Dirack 12/07/2019
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+$user="root";
+$senha="12345";
+
+if ((!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) || ($_SERVER['PHP_AUTH_USER']!=$user || $_SERVER['PHP_AUTH_PW']!=$senha)){
+	header('HTTP/1.1 401 Unauthorized');
+	header('WWW-Authenticate: Basic realm="Guitar Wars"');
+	exit("<h2>Você precisa estar logado para acessar a página do ADMIN.</h2>");
+}
+
+unset($_SERVER['PHP_AUTH_USER']);
+unset($_SERVER['PHP_AUTH_PW']);
+?>
