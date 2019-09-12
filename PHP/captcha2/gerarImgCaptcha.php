b8b70b72f12b7dd7762a872c099fe069809fd5af
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-09-11 22:47:30 -0300

    estudo sobre validação com captcha
    Neste estudo faço a validação de um formulário com captcha em PHP

---
+++ PHP/captcha2/gerarImgCaptcha.php
@@ -0,0 +1,46 @@
+<?php
+/*
+	 gerarImgCaptcha.php (PHP)
+	 
+	 Objetivo: Gerador de imagem captcha com PHP.
+	 
+	 Site: http://www.dirackslounge.online
+	 
+	 Versão 1.0
+	 
+	 Programador: Rodolfo Dirack 11/09/2019
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+define('CAPTCHA_WIDTH',100);
+define('CAPTCHA_HEIGHT',100);
+
+$string="12345";
+
+$img =  imagecreatetruecolor(CAPTCHA_WIDTH,CAPTCHA_HEIGHT);
+$bgcolor = imagecolorallocate($img, 255, 255, 255); // branco
+$graphic_color = imagecolorallocate($img, 64, 64, 64); // cinza-escuro
+$text_color = imagecolorallocate($img, 0, 0, 0); // preto
+
+imagefilledrectangle($img,0,0,100,100,$bgcolor);
+
+for($i=0;$i<5;$i++){
+	imageline($img,0,rand() % CAPTCHA_HEIGHT,rand() % CAPTCHA_WIDTH,rand() % CAPTCHA_HEIGHT,$graphic_color);
+}
+
+for($i=0;$i<50;$i++){
+	imagesetpixel($img, rand() % CAPTCHA_WIDTH,rand() % CAPTCHA_HEIGHT,$graphic_color);
+}
+
+imagettftext($img, 18, 0, 5, CAPTCHA_HEIGHT - 50, $text_color, "./sewer.ttf", $string);
+
+header("Content-type: image/png");
+
+imagepng($img);
+imagedestroy($img);
+	 
+	 
+?>
---
+++ PHP/captcha2/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!--
+	index.php (PHP)
+	
+	Objetivo: Estudo sobre desenvolvimento de captcha em PHP.
+	
+	Site: http://www.dirackslounge.online
+	
+	Versão 1.0
+	
+	Programador: Rodolfo Dirack 11/09/2019
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+-->
+<head>
+	<meta charset="utf-8">
+	<title>Estudo sobre desenvolvimento de captcha em PHP</title>
+</head>
+<body>
+
+	<h1>Formulário de cadastro</h1>
+	<form method="POST">
+		Usuário: <input type="text" name="nome"><br>
+		Email: <input type="text" name="email"><br>
+		Senha: <input type="text" name="senha"><br>
+		<img src="gerarImgCaptcha.php" alt="Validador captcha"><br>
+		<input type="submit" value="Cadastrar"><br>
+
+	</form>
+<?php
+
+?>
+</body>
+</html>
