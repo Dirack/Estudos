eec8078cab33f8eff5a9b46428c5a1163d77aaa6
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-04-15 06:07:31 -0300

    Correção automatizada de exercício
    Correção automatizada de exercício com arquivo Makefile. Utilize o comando
    'make test' para acionar os testes automatizados definidos em 'test.h'.
    
    O arquivo cabeçalho deve ser incluído no arquivo de exercício 'exercicio.h'.
    Ao rodar o 'make test' os testes são realizados

---
+++ C/alloc/test.h
@@ -0,0 +1,42 @@
+/*
+* test.c (C)
+* 
+* Objetivo: Configuração dos testes automatizados para correção dos exercícios.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 15/04/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
+#include <stdio.h>
+#include "../minunit.h"
+
+static int antes = 0;
+static int depois = 0;
+
+void test_setup(void){
+	antes = 4;
+	depois = 0;
+}
+
+void test_teardown(void){
+	/* Nothing */
+}
+
+MU_TEST(test_check){
+	mu_check(antes == 4);
+	mu_check(depois == 0);
+}
+
+MU_TEST_SUITE(test_suite){
+	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+	MU_RUN_TEST(test_check);
+}
