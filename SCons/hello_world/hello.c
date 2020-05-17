7fc939c3c3ea900a6a1cca6b39c9c13439964a75
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-05-16 23:03:25 -0300

    Estudo olá mundo, scons

---
+++ SCons/hello_world/hello.c
@@ -0,0 +1,22 @@
+/*
+* hello.c (C)
+* 
+* Objetivo: Exibir mensagem olá mundo na tela.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 16/05/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Olá mundo, SCons!\n");
+}
