dd383748fb2f836f9ec880cb39d60c359ce085ba
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-06-03 16:54:18 -0300

    Suite de testes do junit-console
    Estudo sobre TDD com junit, exemplo de
    implementação de uma pilha.

---
+++ Java/TDD/pilha/TestPilha.java
@@ -0,0 +1,25 @@
+/*
+	 TestPilha.java (Java)
+	 
+	 Objetivo: Estudo sobre TDD com junit.
+	 
+	 Site: https://dirack.github.io
+	 
+	 Versão 1.0
+	 
+	 Programador: Rodolfo A C Neves (Dirack) 03/06/2020
+	 
+	 Email: rodolfo_profissional@hotmail.com
+	 
+	 Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+import org.junit.Assert.*;
+import junit.framework.TestCase;
+
+public class TestPilha extends TestCase{
+
+	public void pilhaVazia(){
+
+	}
+}
