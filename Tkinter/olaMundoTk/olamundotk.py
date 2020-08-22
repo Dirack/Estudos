527e0a79e4f7d434cb80395e93c223f5df393642
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-08-22 15:21:27 -0300

    Estudo sobre tkinter, olá mundo

---
+++ Tkinter/olaMundoTk/olamundotk.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# olamundotk.py (Python)
+# 
+# Objetivo: Estudo olá mundo TKinter.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 22/08/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+from tkinter import *
+
+menu = Tk()
+menu.title("Hello world, Tkinter!")
+menu.mainloop()
