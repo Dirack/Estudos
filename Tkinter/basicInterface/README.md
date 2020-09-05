29cb8c11b37ee436a2116c4fa060a388760be4bf
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-09-04 22:12:17 -0300

    Tutorial básico de Tkinter do DevMedia
    Criar a primeira interface com Tkinter.
    Disponível em: https://www.devmedia.com.br/tkinter-interfaces-graficas-em-python/33956

---
+++ Tkinter/basicInterface/README.md
@@ -0,0 +1,5 @@
+#### Tutorial básico, criando uma interface no Tkinter
+
+Retirado de [Tutorial DevMedia](https://www.devmedia.com.br/tkinter-interfaces-graficas-em-python/33956)
+
+
---
+++ Tkinter/basicInterface/basic/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# app.py (Python)
+# 
+# Objetivo: Estudo sobre interface em Tkinter.
+# Gerar uma interface básica com o Tkinter.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 04/09/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+from Tkinter import *
+
+class Application:
+    def __init__(self, master=None):
+        pass
+root = Tk()
+Application(root)
+root.mainloop()
