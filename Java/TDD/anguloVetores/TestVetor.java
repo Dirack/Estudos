81aa0887031e1daf4ba48cf4a6354c5bad909685
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-10-21 22:11:43 -0300

    Estudo calcular ângulo entre dois vetores com Java

--- Java/TDD/anguloVetores/TestVetor.java
+++ Java/TDD/anguloVetores/TestVetor.java
@@ -29,10 +29,20 @@ public class TestVetor{
 	public void testCalculaAnguloEntreVetores(){
 		double x1 = 0.0, x2 = 1.0;
 		double y1 = 1.0, y2 = 0.0;
+		double x3 = 1.0, y3 = 1.0;
 		Vetor v1 = new Vetor(x1,y1);
 		Vetor v2 = new Vetor(x2,y2);
+		Vetor v3 = new Vetor(x3,y3);
+
 		/* ângulo entre v1 e v2 deve ser PI/2*/
-		assertEquals((Math.PI/2.),v1.calculaAnguloEntreVetores(v2),0.1);
+		assertEquals((Math.PI/2.),v1.calculaAnguloEntreVetores(v2),0.01);
+
+		/* ângulo entre dois vetores iguais deve ser 0 */
+		assertEquals(0.0,v1.calculaAnguloEntreVetores(v1),0.01);
+		assertEquals(0.0,v2.calculaAnguloEntreVetores(v2),0.01);
+
+		/* ângulo entre os vetores v1 e v3 deve ser PI/4 */
+		assertEquals(0.785,v3.calculaAnguloEntreVetores(v1),0.01);
 	}
 
 }
