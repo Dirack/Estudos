fbf1b623ba80baae89c499d66ec52e3cec340221
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-11-03 14:57:03 -0300

    Estudo sobre alocação dinâmica personalizada

---
+++ C/alloc/general_alloc/alloc/_alloc.h
@@ -0,0 +1,32 @@
+/*
+* _alloc.h (C)
+* 
+* Objetivo: Allocador dinâmico personalizado.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 03/11/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdlib.h>
+
+void* alloc(size_t n, size_t size){
+
+	void* ptr;
+	size *= n;
+
+	if(0>=size)
+		fprintf(stderr,"%s: illegal allocation (%lu bytes)\n",__FILE__,size);
+
+	ptr = malloc(size);
+	if(ptr==NULL)
+		fprintf(stderr,"%s: cannot allocate %lu bytes\n",__FILE__,size);
+
+	return ptr;
+}
---
+++ C/alloc/general_alloc/alloc/exemplo.c
@@ -0,0 +1,35 @@
+/*
+* exemplo.c (C)
+* 
+* Objetivo: Estudo sobre allocação dinâmica personalizada.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 03/11/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
+#include <stdio.h>
+#include "_alloc.h"
+
+int main(void){
+
+	int i;
+	float* array;
+
+	array = (float*) alloc(5,sizeof(float));
+
+	for(i=0;i<5;i++){
+		array[i]=(float)i;
+		printf("%f\n",array[i]);
+	}
+
+	free(array);
+	array = (float*) alloc(5,-1);
+}
