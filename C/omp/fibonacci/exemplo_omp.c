15d3cd12924e28652a74d124a27f23f7c8132c8a
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-11-04 14:01:00 -0300

    Estudo sobre recursão Fibonacci em C com openmp

---
+++ C/omp/fibonacci/exemplo_omp.c
@@ -0,0 +1,41 @@
+/*
+* exemplo_omp.c (C)
+* 
+* Objetivo: Estudo sobre recursão fibonacci com openmp.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 04/11/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdio.h>
+
+#define N 42
+
+long fib(long n) {
+  long i, j;
+
+  if (n < 2) {
+    return n;
+  }
+  else {
+    #pragma omp parallel sections
+    {
+      #pragma omp section
+      i = fib(n-1);
+      #pragma omp section
+      j = fib(n-2);
+    }
+    return i + j;
+  }
+}
+
+int main() {
+  printf("\nFibonacci(%lu) = %lu\n",(long)N,fib((long)N));
+}
