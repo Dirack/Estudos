21c3a24c5615c911e58cba1972ba6b933a0bddb4
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-11-26 13:58:36 -0300

    Estudo matriz simétrica

--- C/matrizSimetrica/main.c
+++ C/matrizSimetrica/main.c
@@ -3,16 +3,14 @@
 #include "matriz.h"
 
 int main() {
-    int i, j, k;
-    
+
     MatrizSimetrica* mat = cria(2);
     
-    atribui(mat, i, j, 1);
-    atribui(mat, i, j, 2);
-    atribui(mat, i, j, 4);
-    atribui(mat, i, j, 7);
+    atribui(mat, 0, 0, 1);
+    atribui(mat, 0, 1, 2);
+    atribui(mat, 1, 1, 7);
  
-    if (acessa(mat, i, j) == mat->v[k]) {
+    if (acessa(mat, 0, 1) == acessa(mat,1,0)) {
         printf("matriz simetrica.\n");
     } else {
         printf("matriz nao simetrica.\n");
