4f47002afbd6ae8a4e29641662c6280293f51b39
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-03-30 20:11:43 -0300

    Exemplo floor e ceil

---
+++ C/floor_ceil/exemplo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+	float x=2.55;
+	
+	printf("Arredonda x para baixo: x=%f\n",floor(x));
+	printf("Arredonda x para cima: x=%f\n",ceil(x));
+}
