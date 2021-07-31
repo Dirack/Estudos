32f60a870ef65592334ebb7a64a884a87b34739e
Dirack <rodolfo_profissional@hotmail.com>
2021-07-31 16:28:48 -0300

    Exemplo paralelização do código da integral

---
+++ C/openMP/integral/original/exemplo.c
@@ -0,0 +1,38 @@
+/*
+* exemplo.c (C)
+* 
+* Objetivo: Programa original para ser paralelizado com openmp.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 31/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdio.h>
+
+static long num_steps = 100000;
+double step;
+
+int main(void){
+	int i;
+	double x;
+	double pi;
+	double sum=0.0;
+
+	step = 1/(double)num_steps;
+
+	for(i=0;i<num_steps;i++){
+		x=(i+0.5)*step;
+		sum = sum+4.0/(1.0+x*x);
+	}
+
+	pi = step*sum;
+
+	printf("pi=%f\n",pi);
+}
