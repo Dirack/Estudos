7379da0412f3d4eb2a3fa75788f6160b1087fc79
Dirack <rodolfo_profissional@hotmail.com>
2022-04-14 22:10:59 -0300

    Atualiza a aula sobre mustache no nodejs

--- nodejs/mod1-nocoes__basicas__node/mustache/src/index.ts
+++ nodejs/mod1-nocoes__basicas__node/mustache/src/index.ts
@@ -12,6 +12,13 @@ console.log(path.join(__dirname,'../public'));
 server.use(express.static(path.join(__dirname,'../public')));
 
 server.use('/',(req: Request, res: Response)=>{
+	let age = 50;
+	let showOld = false;
+
+	if(age>30){
+		showOld = true;
+	}
+
 	let dirack = {
 		name: 'Rodolfo',
 		age: '30'
@@ -19,7 +26,19 @@ server.use('/',(req: Request, res: Response)=>{
 	res.render('home',{
 		user: 'Dirack',
 		dirack,
-		fulano: {name:'fulano',age:'90'}
+		fulano: {name:'fulano',age:'90'},
+		showWelcome: false,
+		showOld,
+		products: [
+			{title:'product X',price:10},
+			{title:'product Y',price:15},
+			{title:'product Z',price:40}
+		],
+		lista: [
+		"frase motivacional inútil",
+		"sua vida não tem sentido"
+		],
+		lista2:[]
 	});
 });
 
