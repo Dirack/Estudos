6a4ceab915766849f815bbde6b3690567ba5b972
Dirack <rodolfo_profissional@hotmail.com>
2022-05-13 14:48:55 -0300

    Atualiza estudo banco de dados com nodejs

--- nodejs/mod3-banco__de__dados/consulta/src/controllers/homeController.ts
+++ nodejs/mod3-banco__de__dados/consulta/src/controllers/homeController.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express';
 import { Product } from '../models/Product';
 import { User } from '../models/User';
+import {Op} from 'sequelize';
 
 export const home = async (req: Request, res: Response)=>{
 
-	let users = await User.findAll();
+	let users = await User.findAll({
+		attributes:['name','age']
+	});
 	console.log(JSON.stringify(users));
+
+	let auser = await User.findAll({
+		where:{age:31}
+	});
+
+	let ouser = await User.findAll({
+		where:{
+			[Op.or]:[{name:'Testador'},{age:31}]
+		}
+	});
 
     let age: number = 90;
     let showOld: boolean = false;
@@ -24,6 +37,8 @@ export const home = async (req: Request, res: Response)=>{
         products: list,
         expensives: expensiveList,
         frasesDoDia: [],
-	users
+	users,
+	auser,
+	ouser
     });
 };
