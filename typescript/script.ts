a7a2ee9d4d5a33e8d34aae78da3a3e6d9c71f0be
Dirack <rodolfo_profissional@hotmail.com>
2022-07-18 15:40:30 -0300

    Estudo typescript código simples

---
+++ typescript/script.ts
@@ -0,0 +1,12 @@
+let numero1 = document.getElementById('numero1') as HTMLInputElement;
+let numero2 = document.getElementById('numero2') as HTMLInputElement;
+var botao = document.getElementById('botao');
+var res = document.getElementById('resultado');
+
+function calcular(n1: number,n2: number){
+	return n1+n2;
+}
+
+botao.addEventListener('click',function(){
+	res.innerHTML = calcular(+numero1.value,+numero2.value).toString();
+});
