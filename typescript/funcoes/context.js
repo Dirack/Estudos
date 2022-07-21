d8f0405f952d23b97e292620490863ced3433491
Dirack <rodolfo_profissional@hotmail.com>
2022-07-21 20:33:54 -0300

    Exemplo funções em typescript

---
+++ typescript/funcoes/context.js
@@ -0,0 +1,9 @@
+var nomes = ['capa', 'docia', 90];
+nomes.forEach(function (nome) {
+    if (typeof nome === 'string') {
+        console.log(nome.toUpperCase());
+    }
+    else {
+        console.log(nome);
+    }
+});
--- typescript/funcoes/script.ts
+++ typescript/funcoes/script.ts
@@ -1,8 +1,10 @@
-function firstLetter(nome: string){
+function firstLetter(nome: string): string{
 	let first = nome.charAt(0).toUpperCase();
 	return first+nome.substring(1);
 }
 
 // firstLetter(90); // Não irá funcionar em typescript
 
-firstLetter('capa');
+let result: string = firstLetter('capa');
+
+console.log(result);
