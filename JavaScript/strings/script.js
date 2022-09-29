407be90c0d95e926637b2347690f7c45833ed109
Dirack <rodolfo_profissional@hotmail.com>
2022-09-29 13:49:01 -0300

    Métodos de string em JS

---
+++ JavaScript/strings/script.js
@@ -0,0 +1,21 @@
+nome="capa docia"
+
+console.log(nome.length,nome.indexOf(' '))
+
+console.log(nome.slice(2,5))
+console.log(nome.slice(2))
+console.log(nome.slice(-2))
+console.log(nome.substring(2))
+console.log(nome.substring(2,5)) // Início, final (sem negativo)
+console.log(nome.substr(2,4)) // Início, quantidade
+console.log(nome.substr(-4,4)) // Início, quantidade
+
+exemplo=" Um texto qualquer "
+
+console.log(exemplo.replace("qualquer","legal"))
+console.log(exemplo.toUpperCase())
+console.log(exemplo.toLowerCase())
+console.log(exemplo.trim())
+console.log(exemplo.charAt(2))
+console.log(exemplo.split(' '))
+
