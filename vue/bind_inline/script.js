3f8ab8752c4aae47d75f71aa9e9ba0ca17af8998
Dirack <rodolfo_profissional@hotmail.com>
2023-07-15 16:17:41 -0300

    Estudo vuejs

---
+++ vue/bind_inline/script.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+	el:'#app',
+	data:{
+		aviso:'Alguma mensagem de aviso',
+		avisoBase:{
+			border:'1px solid gray',
+			fontSize:'20px',
+			padding: '10px'
+		},
+		avisoError:{
+			backgroundColor:'red',
+			color:'white',
+			fontSize:'30px'
+		}
+	},
+
+
+});
