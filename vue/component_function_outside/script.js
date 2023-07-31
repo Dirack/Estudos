4b04b7a76b7d525f841ef8f92374d271cf2746b4
Dirack <rodolfo_profissional@hotmail.com>
2023-07-31 11:43:36 -0300

    Estudo vuejs funções fora do componente

---
+++ vue/component_function_outside/script.js
@@ -0,0 +1,31 @@
+let postagem = {
+	props: ['titulo'],
+	methods:{
+		responder:function(){
+			this.$emit('responder',this.titulo)
+		}
+	},
+	template: `
+		<div>
+			<h2>{{titulo}}</h2>
+			<p><slot></slot></p>
+			<button v-on:click="responder">Responder</button>
+		</div>`
+}
+
+let conteudo = new Vue({
+	el:'#conteudo',
+	data:{
+		responderAberto:false,
+		respostaTitulo:''
+	},
+	methods:{
+		abrirResposta:function(titulo){
+			this.responderAberto=true
+			this.respostaTitulo=titulo
+		}
+	},
+	components:{
+		postagem
+	}
+})
