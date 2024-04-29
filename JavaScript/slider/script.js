7b8875ad3ffb85ba4afcd23835f818263ac5708e
Dirack <rodolfo_profissional@hotmail.com>
2024-04-28 21:05:53 -0300

    Estudo slider javascript completo

---
+++ JavaScript/slider/script.js
@@ -0,0 +1,32 @@
+totalSlides = document.querySelectorAll('.slider--item').length
+
+larguraTela = document.querySelector('.slider').clientWidth
+
+document.querySelector('.slider--width').style.width = `${larguraTela*totalSlides}px`
+
+currentSlide = 0
+
+document.querySelectorAll('.controls button')[0].addEventListener('click',goPrev)
+
+document.querySelectorAll('.controls button')[1].addEventListener('click',goNext)
+
+function goPrev(){
+    if(currentSlide<=0){
+        currentSlide = totalSlides - 1
+    }else{
+        currentSlide--
+    }
+    document.querySelector('.slider--width').style.marginLeft = `${-currentSlide*larguraTela}px`
+    console.log(totalSlides)
+}
+
+function goNext(){
+    if(currentSlide>=2){
+        currentSlide = 0
+    }else{
+        currentSlide++
+    }
+    document.querySelector('.slider--width').style.marginLeft = `${-currentSlide*larguraTela}px`
+    console.log(larguraTela)
+    console.log(currentSlide,totalSlides)
+}
