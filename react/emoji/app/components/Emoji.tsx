a105f92bf4c33443078591f1b5d27ec6d162870a
Dirack <rodolfo_profissional@hotmail.com>
2024-08-27 21:03:01 -0300

    Estudo react js emoji

---
+++ react/emoji/app/components/Emoji.tsx
@@ -0,0 +1,25 @@
+type Props = {
+    rate: number;
+}
+
+const Emoji = ({rate}:Props)=>{
+
+    if (rate > 5){
+        rate = 5
+    }else if (rate < 0){
+        rate = 0
+    }
+
+    rate = Math.floor(rate)
+    let emo = '😃'.repeat(rate) + '😢'.repeat(5-rate)
+    
+    return (
+        <div className="flex justify-center items-center text-6xl">
+            <div className="bg-gray-700 text-white p-2 rounded">{rate.toFixed(1)}</div>
+            <div className="ml-3">{emo}</div>
+        </div>
+        
+    )
+}
+
+export default Emoji
---
+++ react/emoji/app/page.tsx
@@ -0,0 +1,11 @@
+import Emoji from "./components/Emoji"
+
+const Page = ()=>{
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Emoji rate={3.5}/>
+    </div>
+  )
+}
+
+export default Page
