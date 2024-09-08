b9f26282eb907e16a7523a0da80c0c4b2a601295
Dirack <rodolfo_profissional@hotmail.com>
2024-09-08 13:24:15 -0300

    Estudo responsividade no tailwindcss

---
+++ tailwind/responsividade/app/page.tsx
@@ -0,0 +1,9 @@
+const Page = ()=>{
+  return (
+    <div className="container mx-auto bg-blue-200 sm:bg-blue-600 md:bg-green-600">
+      Esse é um texto aleatório e inútil
+    </div>
+  )
+}
+
+export default Page
