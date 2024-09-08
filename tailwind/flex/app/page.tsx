994c9b7c96c74ef0f4b3aa093dcbc9c3682c82c1
Dirack <rodolfo_profissional@hotmail.com>
2024-09-08 15:15:51 -0300

    Estudo display flex no tailwindcss

---
+++ tailwind/flex/app/page.tsx
@@ -0,0 +1,12 @@
+const Page = ()=>{
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 sm:flex-row">
+      <div className="p-4 text-white bg-blue-500 rounded">01</div>
+      <div className="order-first p-8 text-white bg-green-500 rounded">02</div>
+      <div className="p-11 text-white bg-blue-500 rounded">03</div>
+      <div className="p-4 text-white bg-blue-500 rounded">04</div>
+    </div>
+  )
+}
+
+export default Page
