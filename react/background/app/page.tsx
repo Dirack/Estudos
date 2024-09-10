6903384961ec8ecf549f3a332f1a00a608255a27
Rodolfo Neves <rodolfo.neves@eliis.fr>
2024-09-10 09:55:22 -0300

    Estudo sobre background no tailwindcss

---
+++ react/background/app/page.tsx
@@ -0,0 +1,54 @@
+const Page = ()=>{
+ return (
+  <div className="container mx-auto">
+    <div className="block w-60 h-60 bg-center bg-[url(https://png.pngtree.com/back_origin_pic/04/21/79/9422c83b789da2970b127c7b9fd5af89.jpg)]">
+
+    </div>
+
+    <div className="w-screen h-80 bg-repeat bg-[url(https://th.bing.com/th/id/R.50fb110cf8778810b9a7d9dd9f399ee2?rik=hxhcflhZxCEpvQ&pid=ImgRaw&r=0)]">
+
+    </div>
+
+    <div className="w-screen h-80 bg-cover bg-center bg-[url(https://png.pngtree.com/back_origin_pic/04/21/79/9422c83b789da2970b127c7b9fd5af89.jpg)]">
+
+    </div>
+
+    <div className="w-60 h-60 bg-center text-white overflow-y-scroll bg-local bg-[url(https://png.pngtree.com/back_origin_pic/04/21/79/9422c83b789da2970b127c7b9fd5af89.jpg)]">
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+      Conteudo qualquer
+    </div>
+  </div>
+ ) 
+}
+
+export default Page
