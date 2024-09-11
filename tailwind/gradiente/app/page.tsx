8f5e2ae92b6dc1c79fe852c3118c4ca6269e3f19
Dirack <rodolfo_profissional@hotmail.com>
2024-09-10 21:57:23 -0300

    Estudo sobre gradiente em tailwindcss

---
+++ tailwind/gradiente/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Page = ()=>{
+  return (
+    <div className="container mx-auto">
+      <div className="w-60 h-60 bg-gradient-to-r from-sky-500 to-indigo-500"></div>
+    </div>
+  )
+}
+
+export default Page
