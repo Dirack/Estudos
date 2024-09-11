eed7ea555b77c0df11634c264994f3ed5fa08028
Dirack <rodolfo_profissional@hotmail.com>
2024-09-11 15:14:49 -0300

    Estudo animações com tailwind css

---
+++ tailwind/animacao/app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+const Page = ()=>{
+  return(
+    <>
+    <div className="transition-all duration-300 ease-linear w-60 h-24 bg-green-500 hover:w-full">
+    </div>
+    <br></br>
+    <div className="w-60 h-10 bg-blue-900 hover:animate-spin"></div>
+    </>
+  )
+}
+
+export default Page
