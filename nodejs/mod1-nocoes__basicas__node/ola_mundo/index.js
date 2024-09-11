9c94d29d952678f59195253404c6894fa3d47fff
Dirack <rodolfo_profissional@hotmail.com>
2024-09-11 15:15:36 -0300

    Atualiza nome de pasta

--- nodejs/mod1-nocoes__basicas__node/ola_mundo/index.js
+++
@@ -1,4 +0,0 @@
-let nome = "Dirack";
-let idade = "31";
-
-console.log("Meu nome é "+nome+" e eu tenho "+idade+" anos");
---
+++ nodejs/mod1-nocoes__basicas__node/ola_mundo/src/index.js
@@ -0,0 +1,3 @@
+var nome = "Dirack";
+var idade = "31";
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos");
