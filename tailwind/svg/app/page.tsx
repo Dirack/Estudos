06c594a65db154b7fac78bec9c8a61bedeb1d170
Dirack <rodolfo_profissional@hotmail.com>
2024-09-12 13:40:31 -0300

    Estudo svg com tailwindcss

---
+++ tailwind/svg/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+const Page = ()=>{
+  return (
+    <div className="">
+      <svg className="w-24 h-24 fill-blue-500 stroke-sky-900 stroke-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+      </svg>
+    </div>
+  )
+}
+
+export default Page
