49c011641f32305dd8b1b6adf14c6e049d928b87
Dirack <rodolfo_profissional@hotmail.com>
2024-09-13 12:03:25 -0300

    Exercício loading tailwindcss

---
+++ tailwind/loading/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Page = ()=>{
+  return (
+    <div className="container mx-auto">
+      <div className="w-60 h-60 border-8 border-blue-200 border-r-blue-800 border-t-blue-800 rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Page
