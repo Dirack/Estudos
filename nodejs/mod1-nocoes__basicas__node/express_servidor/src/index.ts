60a206e5fea07c6617bc3e9bb7286aa853700475
Rodolfo Neves <rodolfo.neves@eliis.fr>
2024-09-16 14:25:03 -0300

    Configurações básicas do servidor nodejs com express

--- nodejs/mod1-nocoes__basicas__node/express_servidor/src/index.ts
+++ nodejs/mod1-nocoes__basicas__node/express_servidor/src/index.ts
@@ -1,9 +1,16 @@
-import express, {Request, Response} from 'express';
+import express, {Request, Response, urlencoded} from 'express';
+import helmet from 'helmet';
+import path from 'path'
 
 const server = express();
 
+server.use(helmet())
+server.use(express.json())
+server.use(urlencoded({extended:true}))
+server.use(express.static(path.join(__dirname, '../public')))
+
 server.get('/',(req: Request, res: Response)=>{
-	res.send("Olá mundo, NodeJS!");
+	res.json({"Hello":"world"})
 });
 
 server.listen(3000);
