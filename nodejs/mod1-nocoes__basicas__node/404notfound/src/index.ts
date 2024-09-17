e10228cc7574c4892d8128f4cd1df60527c67f90
Rodolfo Neves <rodolfo.neves@eliis.fr>
2024-09-17 09:35:42 -0300

    Estudo internal server error e 404 no nodejs

--- nodejs/mod1-nocoes__basicas__node/404notfound/src/index.ts
+++ nodejs/mod1-nocoes__basicas__node/404notfound/src/index.ts
@@ -1,9 +1,20 @@
-import express, {Request, Response} from 'express';
+import express, {ErrorRequestHandler, Request, Response} from 'express';
 
 const server = express();
+
+server.use("/error", (req: Request, res: Response)=>{
+	throw new Error("This is a custom error message")
+})
 
 server.use((req: Request, res: Response)=>{
 	res.status(404).send("Not found");
 });
+
+const errorHandler: ErrorRequestHandler = (err,req,res,next)=>{
+	console.log(err)
+	res.status(500).json({error:"internal server error"})
+}
+
+server.use(errorHandler)
 
 server.listen(8081);
