bbb161d65ec63cc0ecd93f93e1cec772a4b32395
Rodolfo Neves <rodolfo.neves@eliis.fr>
2024-09-26 11:09:30 -0300

    Atualiza estudo sobre schema validation com zod no nodejs

--- nodejs/zod/schema_validation/index.ts
+++ nodejs/zod/schema_validation/index.ts
@@ -4,7 +4,8 @@ const schema = z.object({
     name: z.string().min(2),
     email: z.string().email(),
     age: z.number().min(18).max(100),
-    opt: z.number().optional()
+    opt: z.number().optional(),
+    lit: z.literal(90)
 })
 
 type User = z.infer<typeof schema>
@@ -12,7 +13,8 @@ type User = z.infer<typeof schema>
 let data: User = {
     name: 'Fulano',
     email: 'fulano@gmail.com',
-    age: 19
+    age: 19,
+    lit:90
 }
 
 let data2: User = {
