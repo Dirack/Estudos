48222fb91cb172db82422a35752eeab727a3336b
Rodolfo Neves <rodolfo.neves@eliis.fr>
2024-10-29 14:30:42 -0300

    Estudo integração do cesiumjs em aplicação nextjs

---
+++ cesiumjs/meuteste/app/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./page.module.css";
+import MainPage from "./main/page";
+
+export default function Home() {
+  return (
+    <main>
+      {/* <a
+        className={styles['source-link']}
+        href='https://github.com/hyundotio/nextjs-ts-cesium-example'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        GitHub Source link
+      </a> */}
+      <MainPage />
+    </main>
+  );
+}
