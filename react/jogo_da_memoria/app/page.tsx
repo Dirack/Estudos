704b58e3ffe3becf426a48b31bd7a7d929e5b93e
Rodolfo Neves <rodolfo.neves@eliis.fr>
2024-12-09 14:35:02 -0300

    Projeto jogo da memória em reactjs

---
+++ react/jogo_da_memoria/app/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import Image from 'next/image'
+import * as C from './App.styles'
+import logoImage from './assets/devmemory_logo.png'
+import { useState } from 'react'
+import { InfoItem } from './components/InfoItem'
+
+const Page = ()=>{
+
+  const [valor,setValor] = useState(0)
+
+  const update = ()=>{
+    let v = valor+1
+    setValor(v)
+  }
+
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink>
+          <Image src={logoImage} width={200} alt='' priority />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label="Tempo" value='00:00' />
+          <InfoItem label="Movimentos" value={valor.toString()} />
+        </C.InfoArea>
+
+        <button onClick={update}>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
+  )
+}
+
+export default Page
